b1d7a08f37ebeaabb5c39c737bac2d565fee42aa
Azgarp <azgar10090@gmail.com>
2022-04-19 21:38:57 +0530

    Foundation Project

---
+++ src/Main/main.java
@@ -0,0 +1,20 @@
+	import java.util.*;
+
+public class Main {
+	public static void main(String[] args)
+	string s ="dum ";
+	boolean s1=true;
+	boolean s2=false;
+	if s=="dum"
+	System.out.println(s1);
+	else
+	System.out.println(s2);
+
+	stringBuilder s3=new stringBuilder("hello");
+	stringBuffer s4=new stringBuffer("world");
+
+
+
+
+
+}
